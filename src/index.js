2ffc6e86f088e4d3a9c7efe0ebbb2d27969efbfc
Chase Jensen <soccerchase@gmail.com>
2019-06-15 16:34:16 -0600

    Me query to return current user, set the JWT token on the request object for use in GQL

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
+const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+
 require("dotenv").config({ path: ".env" });
 const createServer = require("./createServer");
-const db = require("./db");
 const server = createServer();
 
-// TODO: Use Express middleware to handle cookies (JWT)
 server.express.use(cookieParser());
 
-// TODO: Use Express middleware to populate current user
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
