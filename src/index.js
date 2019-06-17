2d1e252a775dc8ac0b2bafaf9fad39f243716efa
Chase Jensen <soccerchase@gmail.com>
2019-06-17 11:29:54 -0600

    Restructure index and add comments

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 require("dotenv").config({ path: ".env" });
 const createServer = require("./createServer");
+const db = require("./db");
 const server = createServer();
 
 server.express.use(cookieParser());
 
+// decode the JWT so we can get the user Id on each request
 server.express.use((req, res, next) => {
   const { token } = req.cookies;
   if (token) {
     const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the req for future requests to access
     req.userId = userId;
   }
   next();
