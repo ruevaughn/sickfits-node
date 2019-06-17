9f0d88f5773347f32176fa1f9a39e3f95993056f
Chase Jensen <soccerchase@gmail.com>
2019-06-17 11:31:02 -0600

    Refactor method to check if the user is logged in via request

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,14 +1,15 @@
 const { forwardTo } = require("prisma-binding");
+const { isLoggedIn } = require("../../utils/auth");
+const hasPermission = require("../../utils/permissions");
 
 const Query = {
   items: forwardTo("db"),
   item: forwardTo("db"),
   itemsConnection: forwardTo("db"),
   me(parent, args, ctx, info) {
-    // Check if there is a current user id
-    if (!ctx.request.userId) {
-      return null;
-    }
+    // Check if there is a current user
+    if (!isLoggedIn(ctx.request)) return null;
+
     return ctx.db.query.user(
       {
         where: { id: ctx.request.userId }
