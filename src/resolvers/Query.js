cb6fec50fcb1937b49ec5c4f2daf3b2b39c8f6bc
Chase Jensen <soccerchase@gmail.com>
2019-06-17 21:42:31 -0600

    Users query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,6 +1,6 @@
 const { forwardTo } = require("prisma-binding");
 const { isLoggedIn } = require("../../utils/auth");
-const hasPermission = require("../../utils/permissions");
+const { hasPermission } = require("../../utils/permissions");
 
 const Query = {
   items: forwardTo("db"),
@@ -16,6 +16,18 @@ const Query = {
       },
       info
     );
+  },
+  async users(parent, args, { request, db }, info) {
+    // 1. Check if they are logged in
+    if (!isLoggedIn(request))
+      throw new Error("You must be logged in to perform this action");
+
+    // 2. Check if the user has the permission to query all the users
+    hasPermission(request.currentUser, ["ADMIN", "USER", "PERMISSIONUPDATE"]);
+
+    // 3. If they do, query all the users!
+    // Info is going to include the graphql query which has the fields we are requesting on the front end
+    return db.query.users({}, info);
   }
 };
 
